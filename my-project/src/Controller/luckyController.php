d470035c72e98a84e1fa40dc2d630fc8c44e0d6b
daiana.costreie <daiana.costreie@gmail.com>
2019-07-25 15:25:56 +0300

    basic website

---
+++ my-project/src/Controller/luckyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class luckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky")
+     * @param Request $request
+     * @return Response
+     */
+    public function luckyNumber(Request $request)
+    {
+        $number = rand(1,100);
+
+        return $this->render(
+            'lucky.html.twig',
+            ['luckyNumber' => $number]
+        );
+    }
+}
